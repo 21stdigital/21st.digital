3653f6f50ab30b656bdd0b6bce274a592a7f317c
Fabian Pahl <fabian@21st.digital>
2021-03-27 19:22:19 +0100

    Fixed copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ import ContactButton from './ContactButton'
 
 const Footer = () => (
   <footer className="pt-36 w-full flex flex-col items-center justify-center bg-black">
-    <div className="sm:w-2/3 w-full px-4 text-white flex flex-col">
+    <div className="w-full sm:w-2/3 max-w-5xl px-4 text-white flex flex-col">
       <div className="w-full text-3xl sm:text-4xl md:text-5xl font-bold">
         <h2 className="w-full md:w-2/3">Wie sieht Erfolg für Dein Projekt aus?</h2>
       </div>
       <div className="flex mt-8 flex-col md:flex-row md:justify-between items-start">
         <p className="w-full md:w-2/3 text-gray-400">
-          Gerne lernen wir Dich und Dein Projekt kennen und sind gespannt, was Du vorhast und wie wir Dir helfen können.
+          Wir helfen Dir nicht einfach nur Features umzusetzen sondern das richtige Produkt mit der richtigen Strategie.
         </p>
         <div className="w-full md:w-44 pt-6 md:pt-0">
           {typeof window !== 'undefined' ? <ContactButton title="Schreib' uns" /> : null}
