d5c4baaaec9238ff52d71a1fba4fee05514cb586
Steffen Rüther <steffen@21st.digital>
2024-09-20 14:30:13 +0200

    chore: improve typo styling

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,12 +1,12 @@
 @cascade(['overline', 'page_title', 'intro_text'])
-<header>
+<header class="page-layout">
     <div class="layout-content my-220 grid-cols grid">
         <p
             class="text-h6 col-start-1 -col-end-1 mb-8 text-center sm:col-start-2 sm:-col-end-2 xl:col-start-4 xl:-col-end-4">
             {{ $overline }}
         </p>
         <h1
-            class="text-h1 text-highlight col-start-1 -col-end-1 text-center sm:col-start-2 sm:-col-end-2">
+            class="text-hero text-highlight col-start-1 -col-end-1 text-balance text-center sm:col-start-2 sm:-col-end-2 lg:col-start-1 lg:-col-end-1">
             {{ $page_title ?? $title }}
         </h1>
         <p
