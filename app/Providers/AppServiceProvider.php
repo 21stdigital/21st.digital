822777c0a3dbfe0bd48529bf1e3acd27b0f2050c
Steffen Rüther <steffen@21st.digital>
2024-10-15 13:28:37 +0200

    feat: extend experts collection with calendly field
    ✨ (AppServiceProvider.php): add computed property for experts' calendly URL
    
    📝 (experts): add new expert profiles for Fabian Pahl and Steffen Rüther
    
    📝 (expert.yaml): update blueprint to include image field for experts
    
    📝 (person.yaml): add email and calendly_handle fields with validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Statamic\Facades\Collection;
 use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
 
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
             $event->extendSocialite('google', \SocialiteProviders\Google\Provider::class);
+        });
+
+        Collection::computed('experts', 'calendly_url', function ($entry, $value) {
+            return "https://calendly.com/{$entry->calendly_handle}";
         });
     }
 }
