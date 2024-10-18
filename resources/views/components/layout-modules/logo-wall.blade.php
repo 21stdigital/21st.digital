d0450ee0ffe99926c73f1f7d7c9d42603d273208
Steffen Rüther <steffen@21st.digital>
2024-10-18 14:30:13 +0200

    chore: remove dumy data from logo wall

--- resources/views/components/layout-modules/logo-wall.blade.php
+++ resources/views/components/layout-modules/logo-wall.blade.php
@@ -1,40 +1,8 @@
 @props([
-    'overline' => 'Unsere Kunden',
-    'headline' => 'Vertrauen und Zusammenarbeit',
-    'intro_text' =>
-        'Diese Unternehmen vertrauen auf unsere Expertise, um ihre digitalen Visionen erfolgreich umzusetzen.',
-    'clients' => [
-        [
-            'title' => 'Bayer',
-            'url' => 'https://www.bayer.com',
-            'logo' => 'icons/logos/bayer.svg'
-        ],
-        [
-            'title' => 'Ensemble Resonanz',
-            'url' => 'https://www.ensembleresonanz.com',
-            'logo' => 'icons/logos/ensemble-resonanz.svg'
-        ],
-        [
-            'title' => 'FIEGE Logistik',
-            'url' => 'https://fiege.com',
-            'logo' => 'icons/logos/fiege.svg'
-        ],
-        [
-            'title' => 'Deutsche Telekom',
-            'url' => 'https://telekom.com',
-            'logo' => 'icons/logos/deutsche-telekom.svg'
-        ],
-        [
-            'title' => 'HYROX',
-            'url' => 'https://hyrox.com',
-            'logo' => 'icons/logos/hyrox.svg'
-        ],
-        [
-            'title' => 'Sportfive',
-            'url' => 'https://sportfive.com',
-            'logo' => 'icons/logos/sportfive.svg'
-        ]
-    ]
+    'overline' => null,
+    'headline' => null,
+    'intro_text' => null,
+    'clients' => []
 ])
 <section {{ $attributes->class(['layout-content']) }}>
     @if ($headline)
