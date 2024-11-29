0445c348c95ec8db22720cb93505a3313927b8b3
Stefan Gruna <2184676+Sm1lEE@users.noreply.github.com>
2024-11-29 16:16:16 +0100

    fix: svg handling in safari (#395)

--- resources/views/components/elements/info-box.blade.php
+++ resources/views/components/elements/info-box.blade.php
@@ -3,7 +3,7 @@
     @if ($logo)
         {!! Statamic::tag('svg')->params([
             'src' => $logo,
-            'class' => 'w-[12rem] mb-20'
+            'class' => 'max-w-[12rem] mb-20 max-h-[4.2rem] h-full'
         ]) !!}
     @endif
     <x-text :text="$text" />
