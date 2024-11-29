dd37d32bd2f16989e23d1587e7a572d0e7ca99f4
Stefan Gruna <stefan@21st.digital>
2024-11-29 16:14:07 +0100

    fix: svg logo in quote module not showing in safari

--- resources/views/components/layout-modules/quote.blade.php
+++ resources/views/components/layout-modules/quote.blade.php
@@ -17,12 +17,12 @@
                     <a href="{{ $client_url }}"
                         class="w-[12rem] sm:flex sm:justify-end md:w-[16.6rem]">
                         <x-utilities.svg :svg="$client_logo"
-                            class="max-h-[4.2rem] max-w-[12rem] md:max-h-[5.8rem] md:max-w-[16.6rem]" />
+                            class="size-full max-h-[4.2rem] max-w-[12rem] md:max-h-[5.8rem] md:max-w-[16.6rem]" />
                     </a>
                 @else
                     <div class="w-[12rem] sm:flex sm:justify-end md:w-[16.6rem]">
                         <x-utilities.svg :svg="$client_logo"
-                            class="max-h-[4.2rem] max-w-[12rem] md:max-h-[5.8rem] md:max-w-[16.6rem]" />
+                            class="size-full max-h-[4.2rem] max-w-[12rem] md:max-h-[5.8rem] md:max-w-[16.6rem]" />
                     </div>
                 @endif
 
