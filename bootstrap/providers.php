c4bb84a02f1abc8e6e1e5be8ae31ad79b5cad6bf
Stefan Gruna <stefan@21st.digital>
2026-03-17 13:04:56 +0100

    chore(deps): update laravel pint and use same version in github workflow

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use App\Providers\HorizonServiceProvider;
+use SocialiteProviders\Manager\ServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\HorizonServiceProvider::class,
-    SocialiteProviders\Manager\ServiceProvider::class,
+    AppServiceProvider::class,
+    HorizonServiceProvider::class,
+    ServiceProvider::class,
 ];
